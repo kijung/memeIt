787974273e5f125b4a00f0007b311211696066a0
Edward Fang <edward.fang@berkeley.edu>
2016-11-09 15:17:31 -0800

    finished reset

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,11 +12,13 @@ class UsersController < ApplicationController
   def create
    @user = User.new(user_params)
    if @user.save
-     # Handle a successful save.
+     flash[:success] = "Success!"
+     redirect_to :back
    else
      render 'new'
    end
  end
+
 
  private
 
