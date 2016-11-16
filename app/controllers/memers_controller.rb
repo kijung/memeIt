db51d4118b2b1d46a46762c8eb8a0538da0270a3
Jordan Li <jordanwyli@gmail.com>
2016-11-16 11:40:23 -0800

    added profile

--- app/controllers/memers_controller.rb
+++ app/controllers/memers_controller.rb
@@ -3,5 +3,7 @@ class MemersController < ApplicationController
   # before_action :authenticate_user!
  def memersHome
  end
-
+ def damage
+    @meme.destroy
+ end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#home'
   get  '/signup',  to: 'users#new'
-  get '/memers/memersHome', to: 'memers#profile'
+  get '/Profile', to: 'memers#memersHome'
   resources :users
 end
